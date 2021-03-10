b4fb82756a1581aa79561e4999e1b142e14ef387
Your Name <you@example.com>
2021-03-10 13:54:00 +0100

    working points and time

--- modules/points/index.js
+++ modules/points/index.js
@@ -16,7 +16,7 @@ const DUMMY = [
 ];
 
 
-const plotTest = (route, timeFormat = "HH:mm.ss") => {
+const plotPoints = (route, timeFormat = "HH:mm.ss") => {
     TIME_FORMAT= timeFormat;
 
     let FEATS = [];
@@ -63,46 +63,4 @@ const plotTest = (route, timeFormat = "HH:mm.ss") => {
     });
 }
 
-
-const plotPoints = (route) => {
-    console.log("HIKE");
-    let i = 0;
-    let FEATURES = [];
-
-    map.addSource('points', {
-        'type': 'geojson',
-        'data': {
-            'type': 'FeatureCollection',
-            'features': FEATURES
-        }
-    });
-    map.addLayer({
-        'id': 'points',
-        'type': 'circle',
-        'source': 'points',
-        'paint': {
-            'circle-radius': 3,
-            'circle-color': '#B42222'
-        },
-        'filter': ['==', '$type', 'Point']
-    });
-
-    map.addLayer({
-        'id': 'points-text',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-            'text-field': ['get', 'title'],
-            'text-font': [
-                'Open Sans Semibold',
-                'Arial Unicode MS Bold'
-            ],
-            "text-size": 10,
-            'text-offset': [0.3, 0],
-            'text-anchor': 'left'
-        },
-    });
-}
-
-
-export {plotPoints, plotTest}
+export {plotPoints}
